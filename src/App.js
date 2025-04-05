fad7811ccd9b70407625b37da36965eb900bdf11
Crimsey <kubsok2@wp.pl>
2025-04-05 21:50:23 +0200

    01 branch

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import GoalList from "./components/GoalList/GoalList";
-import NewGoal from "./components/NewGoal/NewGoal";
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 const App = () => { //functional components
-    const [courseGoals, setCourseGoals] = useState([
-        {id: 'cg1', text: 'Uno'},
-        {id: 'cg2', text: 'Dos'},
-        {id: 'cg3', text: 'Tre'}
-    ]);
-
-    const addNewGoalHandler = newGoal => {
-        // W tym przypadku jak wielokrotnie klikniemy akcję courseGoals może
-        // nie być aktulanym ostatnim state'em
-        // setCourseGoals(courseGoals.concat(newGoal));
-
-        //Dlatego (jeśli state aktualny polega na poprzednim):
-        //previousState => new state snapshot
-        // setCourseGoals((prevCourseGoals) => {
-        //     return prevCourseGoals.concat(newGoal);
-        // });
-        setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal));
-    }
 
     return (
-        <div className="course-goals">
-            <h2>Course Goals 2</h2>
-            <NewGoal onAddGoal={addNewGoalHandler}/>
-            <GoalList goals={courseGoals} />
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/" component={Users} exact/>
+                <Route path="/places/new" component={NewPlace} exact/>
+                <Redirect to="/"/>
+            </Switch>
+        </Router>
     );
 };
 
