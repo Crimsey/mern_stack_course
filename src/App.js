6e7033de3f6f281f6f28decfaffcccfb9d98035d
Crimsey <kubsok2@wp.pl>
2025-04-06 01:05:41 +0200

    01 branch - sidedrawer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import './App.css';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => { //functional components
 
     return (
         <Router>
-            <Switch>
-                <Route path="/" component={Users} exact/>
-                <Route path="/places/new" component={NewPlace} exact/>
-                <Redirect to="/"/>
-            </Switch>
+            <MainNavigation/>
+            <main>
+                <Switch>
+                    <Route path="/" component={Users} exact/>
+                    <Route path="/places/new" component={NewPlace} exact/>
+                    <Redirect to="/"/>
+                </Switch>
+            </main>
         </Router>
     );
 };
