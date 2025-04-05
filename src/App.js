2b32cef71343a37a9345b734501d612ebfa4129c
Crimsey <kubsok2@wp.pl>
2025-04-05 02:25:11 +0200

    binding

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import GoalList from "./components/GoalList/GoalList";
 import NewGoal from "./components/NewGoal/NewGoal";
 
 const App = () => { //functional components
-    const courseGoals = [
+    const [courseGoals, setCourseGoals] = useState([
         {id: 'cg1', text: 'Uno'},
         {id: 'cg2', text: 'Dos'},
         {id: 'cg3', text: 'Tre'}
-    ];
+    ]);
 
-    const addNewGoalHandler = (newGoal) => {
-        courseGoals.push(newGoal);
-        console.log(courseGoals);
+    const addNewGoalHandler = newGoal => {
+        // W tym przypadku jak wielokrotnie klikniemy akcję courseGoals może
+        // nie być aktulanym ostatnim state'em
+        // setCourseGoals(courseGoals.concat(newGoal));
+
+        //Dlatego (jeśli state aktualny polega na poprzednim):
+        //previousState => new state snapshot
+        // setCourseGoals((prevCourseGoals) => {
+        //     return prevCourseGoals.concat(newGoal);
+        // });
+        setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal));
     }
 
     return (
