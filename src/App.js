e3e472345cc062d7e5dcd1e8376ca2a12bd47153
Crimsey <kubsok2@wp.pl>
2025-04-06 22:08:32 +0200

    branch 01 - places + buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => { //functional components
@@ -12,6 +13,7 @@ const App = () => { //functional components
             <main>
                 <Switch>
                     <Route path="/" component={Users} exact/>
+                    <Route path="/:userId/places" component={UserPlaces} exact/>
                     <Route path="/places/new" component={NewPlace} exact/>
                     <Redirect to="/"/>
                 </Switch>
